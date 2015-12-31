1744972026de9cf27c8a7dc39cf39cd83d5f922f
Sergey Sharybin <sergey.vfx@gmail.com>
2015-12-31 15:46:41 +0500

    Initial support of NVRTC routines

--- cuewTest/cuewTest.c
+++ cuewTest/cuewTest.c
@@ -10,6 +10,16 @@ int main(int argc, char* argv[]) {
     printf("CUDA found\n");
     printf("NVCC path: %s\n", cuewCompilerPath());
     printf("NVCC version: %d\n", cuewCompilerVersion());
+    if (nvrtcVersion) {
+        int major, minor;
+        nvrtcVersion(&major, &minor);
+        printf("Found runtime compilation library version %d.%d\n",
+               major,
+               minor);
+    }
+    else {
+        printf("Runtime compilation library is missing\n");
+    }
   }
   else {
     printf("CUDA not found\n");
